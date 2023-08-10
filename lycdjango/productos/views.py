ebb772674f92f2fc24c26820c44e2b115f8f32a5
Sebas0317 <sebastiansandoval12371@gmail.com>
2023-08-10 14:01:39 -0500

    filtros

--- lycdjango/productos/views.py
+++ lycdjango/productos/views.py
@@ -4,7 +4,8 @@ from productos.models import Producto
 import random
 
 def product_list(request):
-    filtro = request.GET.get('filtro', '') 
+    filtro = request.GET.get('filtro', '')
+    precio = request.GET.get('precio', '')
 
     productos = Producto.objects.all()
 
@@ -15,6 +16,20 @@ def product_list(request):
     elif filtro == 'masvendidos':
         productos = productos.order_by('-cantidad_disponible')
 
+    if precio:
+        if precio == '0-20000':
+            productos = productos.filter(precio__range=(0, 20000))
+        elif precio == '20000-40000':
+            productos = productos.filter(precio__range=(20000, 40000))
+        elif precio == '40000-60000':
+            productos = productos.filter(precio__range=(40000, 60000))
+        elif precio == '60000-80000':
+            productos = productos.filter(precio__range=(60000, 80000))
+        elif precio == '80000-100000':
+            productos = productos.filter(precio__range=(80000, 100000))
+        elif precio == '100000':
+            productos = productos.filter(precio__gte=100000)
+
     paginator = Paginator(productos, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -24,6 +39,8 @@ def product_list(request):
     return render(request, 'productos/product_list.html', {
         'page_obj': page_obj,
         'producto_aleatorio': producto_aleatorio,
-        'filtro': filtro  # Pasa el filtro seleccionado al contexto
+        'filtro': filtro,
+        'precio': precio,
     })
+
 

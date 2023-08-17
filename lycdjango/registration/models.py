711c4bb7b8db2b8ba3d05998885de1f767c3bd43
TheReDNooB <xmiguelx209@gmail.com>
2023-08-16 22:55:50 -0500

    ajuste modelo

--- lycdjango/registration/models.py
+++ lycdjango/registration/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+#perfil usuario
+class perfilUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil', verbose_name='Usuario')
+
+
+    class Meta:
+        verbose_name = 'perfil'
+        verbose_name_plural = 'perfiles'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.user.username
+    
+def crear_perfilUser(sender, instance, created, **kwargs):
+    if created:
+        perfilUser.objects.create(user=instance)
+
+def guardar_perdilUser(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(crear_perfilUser, sender=User)
+post_save.conect(guardar_perdilUser, sender=User)

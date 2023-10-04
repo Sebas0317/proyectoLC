709f7757f757001cc3b07424b152becb58f8913d
TheReDNooB <127248097+TheReDNooB@users.noreply.github.com>
2023-10-04 14:53:12 -0500

    Update settings.py
    cambio de password del settings de la email-backend

--- lycdjango/lycdjango/settings.py
+++ lycdjango/lycdjango/settings.py
@@ -124,7 +124,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'comercializadoralyc99@gmail.com'
-EMAIL_HOST_PASSWORD = 'kppr cbkx knty shrv'
+EMAIL_HOST_PASSWORD = 'rzoc miwa gfnc jygq'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 

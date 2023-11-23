97d6a03cc4e65f96a3ec762380e56f840e71fa32
Sebas0317 <sebastiansandoval12371@gmail.com>
2023-11-22 21:53:15 -0500

    mensajes contacto

--- lycdjango/contact/forms.py
+++ lycdjango/contact/forms.py
@@ -4,4 +4,10 @@ from .models import Mensaje
 class MensajeForm(forms.ModelForm):
     class Meta:
         model = Mensaje
-        fields = ['nombre', 'correo', 'mensaje']
+        fields = ['nombre', 'correo', 'asunto', 'mensaje']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'placeholder': 'Ingresa tu nombre'}),
+            'correo': forms.EmailInput(attrs={'placeholder': 'Ingresa tu correo electrónico'}),
+            'asunto': forms.TextInput(attrs={'placeholder': 'Ingresa el asunto'}),
+            'mensaje': forms.Textarea(attrs={'placeholder': 'Escribe tu mensaje aquí'}),
+        }
--- lycdjango/contact/models.py
+++ lycdjango/contact/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Mensaje(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
-    correo = models.EmailField()
+    correo = models.EmailField() 
     asunto = models.CharField(max_length=100, default="Sin Asunto")  # Nuevo campo
     mensaje = models.TextField()
 
